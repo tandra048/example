e5401f9fe219265e572447a75935b4eefa782762
tandra048 <2048tandraju@gmail.com>
2024-04-05 23:06:28 +0600

    second

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,10 +1,29 @@
-
-
+import Navbar from "../Navbar/Navbar";
+import { Link } from "react-router-dom";
 const Login = () => {
     return (
         <div>
-            
-        </div>
+        <Navbar></Navbar>
+        <form>
+        <div className="form-control">
+      <label className="label">
+        <span className="label-text">Email</span>
+      </label>
+      <input type="email" placeholder="enter your email address" className="input input-bordered" required />
+    </div>
+    <div className="form-control">
+      <label className="label">
+        <span className="label-text">Password</span>
+      </label>
+      <input type="password" placeholder="password" className="input input-bordered" required />
+    
+    </div>
+    <div className="form-control mt-6">
+      <button  className="btn btn-primary">Login</button>
+    </div>
+    <p className="text-center mt-8"> Do not have an account? <Link className="text-red-600" to="/register">Register</Link></p>
+  </form>
+    </div>
     );
 };
 
